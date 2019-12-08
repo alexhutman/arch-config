034a8b8061c58421f239ac2116d0b53d1d18191c
Alex <alexhutman@gmail.com>
2019-12-08 16:47:33 -0500

    Added git aliases

--- zsh/zshrc_files/aliases.zsh
+++ zsh/zshrc_files/aliases.zsh
@@ -12,4 +12,13 @@ alias vim="nvim"
 # Clear whole screen
 alias clear="clear && printf '\e[3J'"
 
-
+# Git aliases
+alias ga="git add"
+alias gb="git branch"
+alias gch="git checkout"
+alias gco="git commit"
+alias gd="git diff"
+alias gl="git log"
+alias gm="git merge"
+alias gp="git push"
+alias gs="git status"
