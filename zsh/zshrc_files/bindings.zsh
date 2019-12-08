13463a629dfe0e0ccab1a0cff5bb0993a01b146c
Alex <alexhutman@gmail.com>
2019-12-08 17:27:09 -0500

    Added bindings to search history for the command in the current buffer

--- zsh/zshrc_files/bindings.zsh
+++ zsh/zshrc_files/bindings.zsh
@@ -8,4 +8,9 @@
 # Set vim bindings for zsh
 bindkey -v
 
-
+# Bind Ctrl+K to go forwards (and Ctrl+J to go backwards in the shell history.
+# k and j still search forwards/backwards in the history as normal (in normal mode), 
+# but in insert mode, Ctrl+J and Ctrl+K go forwards and backwards in the history with 
+# respect to the current command in the buffer.
+bindkey "^K" history-beginning-search-backward
+bindkey "^J" history-beginning-search-forward
