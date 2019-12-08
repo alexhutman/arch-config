8522f985044bc1de756b8ecc5a6a2b8d0db6516e
Alex <alexhutman@gmail.com>
2019-12-07 22:37:00 -0500

    Just hardcoding the config path...

--- neovim/vim_to_nvim.sh
+++ neovim/vim_to_nvim.sh
@@ -4,18 +4,9 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Run neovim script that outputs the config directory to a temporary file called "name_of_config_file"
-sed "s|{SCRIPT_PATH}|$SCRIPT_DIR|g" "$SCRIPT_DIR/output_config_dir_template.vim" > "$SCRIPT_DIR/output_config_dir.vim"
-nvim +"so $SCRIPT_DIR/output_config_dir.vim"
-rm "$SCRIPT_DIR/output_config_dir.vim"
 
-# Get rid of the blank lines produced by neovim (not sure why that happens)
-sed -i '/^$/d' "$SCRIPT_DIR/name_of_config_file"
-
-# Set variable for config dir path
-CFG_PATH="$(cat $SCRIPT_DIR/name_of_config_file)"
-
-# Remove the temporary file
-rm name_of_config_file
+# Set variable for config folder path
+CFG_PATH="$(echo ~)/.config/nvim"
 
 # Make config directory
 mkdir -p "$CFG_PATH"
