db17c54b1576c503940c441cd32a565ae40c0663
Alex <alexhutman@gmail.com>
2021-03-14 00:17:00 -0500

    added optional custom test username

--- test/create_user.sh
+++ test/create_user.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Add user, set password and name to test
-sudo adduser test <<- EOF
+# First parameter is the user's name, defaults to "test"
+[ -z "$1" ] && NEW_USER="test" || NEW_USER="$1"
+
+# Add user, set password to "test" and name to supplied user name
+sudo adduser "$NEW_USER" <<- EOF
 	test
 	test
-	test
+	$NEW_USER
 
 
 
@@ -13,4 +16,4 @@ sudo adduser test <<- EOF
 EOF
 
 # Allow user to sudo
-sudo usermod -aG sudo test
+sudo usermod -aG sudo "$NEW_USER"
--- test/del_user.sh
+++ test/del_user.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# First parameter is the user's name, defaults to "test"
+[ -z "$1" ] && DEL_USER="test" || DEL_USER="$1"
+
 # Kill any processes belonging to user
-ACTIVE_PROCS="$(ps -u test | tail -n +2 | awk '{ print $1 }')"
+ACTIVE_PROCS="$(ps -u $DEL_USER | tail -n +2 | awk '{ print $1 }')"
 [ -n "$ACTIVE_PROCS" ] && echo "$ACTIVE_PROCS" | xargs sudo kill -9 
 
 # Remove user and their home directory
-sudo deluser --remove-home test
+sudo deluser --remove-home "$DEL_USER"
